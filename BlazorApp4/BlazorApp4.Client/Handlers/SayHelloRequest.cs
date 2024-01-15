3b649123767f6ed4c4ec1d896250a0c2546b4396
Tomás López Rodríguez <tlopez@hiberus.com>
2024-01-15 12:49:01 +0100

    Fixes

--- BlazorApp4/BlazorApp4.Client/Handlers/SayHelloRequest.cs
+++ BlazorApp4/BlazorApp4.Client/Handlers/SayHelloRequest.cs
@@ -4,5 +4,5 @@ namespace BlazorApp4.Client.Handlers;
 
 public record SayHelloRequest: IRequest<SayHelloRequest.SayHelloResponse>
 {
-    public record SayHelloResponse(string response);
+    public record SayHelloResponse(string Response);
 }
